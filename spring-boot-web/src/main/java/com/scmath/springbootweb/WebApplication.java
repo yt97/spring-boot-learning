fa7addcbaefa0ec6fb5fd5599c9400fe74cda8b7
yt <1565101721@qq.com>
2020-03-15 19:11:49 +0800

    jdbc模块

--- spring-boot-web/src/main/java/com/scmath/springbootweb/WebApplication.java
+++ spring-boot-web/src/main/java/com/scmath/springbootweb/WebApplication.java
@@ -1,10 +1,31 @@
 package com.scmath.springbootweb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+/**
+ * @author yt
+ */
 
 @SpringBootApplication
 public class WebApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        System.out.println("bean----->commandLineRunner(ctx)");
+        // 目的是
+        return args -> {
+            System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
--- spring-boot-web/src/main/java/com/scmath/springbootweb/controller/WebController.java
+++ spring-boot-web/src/main/java/com/scmath/springbootweb/controller/WebController.java
@@ -1,36 +1,21 @@
 package com.scmath.springbootweb.controller;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 
 /**
  * @author yt
- * @version 1.0
  * @date 2019/8/3/003 18:56
  */
 @RestController
+@RequestMapping("/web")
 public class WebController {
 
     @GetMapping("/demo1")
     public String demo1() {
-        System.out.println("WebController----->demo1()");
-        return "Hello! Web";
+        return "this is a simple Web Demo!";
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        System.out.println("WebController里的bean----->commandLineRunner(ctx)");
-        // 目的是
-        return args -> {
-            System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            Arrays.stream(beanNames).forEach(System.out::println);
-        };
-    }
 }
